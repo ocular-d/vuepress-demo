08d01b40ef6d5ca0e4aa461c708f0cb9956b96ea
sven <sven@so36.net>
2020-05-24 15:02:53 +0200

    chore(docs): start with landing page structure

--- docs/.vuepress/components/HomeFeatures.vue
+++ docs/.vuepress/components/HomeFeatures.vue
@@ -9,34 +9,16 @@
       </p>
     </div>
     <div class="feature">
-      <h2>Getting Started</h2>
+      <h2>VuePress</h2>
       <p>
         <a href="/install.html">Install</a><br />
         <a href="/getting-started.html">Hello World in 6 Minutes</a><br />
       </p>
     </div>
     <div class="feature">
-      <h2>Tutorials</h2>
+      <h2>GitHub Actions</h2>
       <p>
         <a href="/tutorial/">Overview</a><br />
-      </p>
-    </div>
-    <div class="feature">
-      <h2>Recipes (Examples)</h2>
-      <p><a href="/recipes/custom-fields.html">Custom Fields</a><br /></p>
-    </div>
-    <div class="feature">
-      <h2>API Reference</h2>
-      <p>
-        <a href="/openapi.html">API Explorer</a><br />
-        <!-- <a href="/postman.html">Postman</a><br /> -->
-      </p>
-    </div>
-    <div class="feature">
-      <h2>Infrastructure</h2>
-      <p>
-        <a href="/infra/activity-audit-log-endpoints.html">Access logs with the API</a><br />
-        <a href="/infra/export-logs.html"> Export logs as JSON</a><br />
       </p>
     </div>
   </div>
